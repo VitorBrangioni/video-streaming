2f105cd5a1fa934fac081fef7bdc859cbda54894
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-03-20 17:45:58 -0300

    Created methods to doing request mapping (GET and POST).

--- src/br/com/vitorbrangioni/videostreaming/controller/VideoController.java
+++ src/br/com/vitorbrangioni/videostreaming/controller/VideoController.java
@@ -1,6 +1,14 @@
 package br.com.vitorbrangioni.videostreaming.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Video Controller 
@@ -10,5 +18,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class VideoController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(VideoController.class);	
 	
+	@RequestMapping(value = "/encoding", method = RequestMethod.POST)
+	public void encoding(HttpSession session, @RequestParam("file") MultipartFile upload) {
+	}
+	
+	@RequestMapping(value = "/encoding", method = RequestMethod.GET)
+	public String enconding() {
+		return "form";
+	}
+
 }
