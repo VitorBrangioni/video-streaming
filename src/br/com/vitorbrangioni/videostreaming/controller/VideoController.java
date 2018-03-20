50f4291ad469948542df51ae761cd30a2c355eb3
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-03-20 17:43:38 -0300

    Class to Video Controller.

---
+++ src/br/com/vitorbrangioni/videostreaming/controller/VideoController.java
@@ -0,0 +1,14 @@
+package br.com.vitorbrangioni.videostreaming.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Video Controller 
+ * @author vitorbrangioni
+ *
+ */
+@Controller
+public class VideoController {
+	
+	
+}
